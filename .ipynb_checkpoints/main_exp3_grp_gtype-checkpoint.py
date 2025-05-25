05aa2cb4f2939fddb1284b300c6872a72fe91836
andresdominguezruiz <andresdominguezruiz.ad@gmail.com>
2025-05-25 10:08:45 +0000

    Exp adicional

--- .ipynb_checkpoints/main_exp3_grp_gtype-checkpoint.py
+++ .ipynb_checkpoints/main_exp3_grp_gtype-checkpoint.py
@@ -21,17 +21,17 @@ HYPER="s"
 index_values=[5,6]
 #er_values=[0.00005,0.00125,0.005,0.01]
 #grp_values=[100,600,1100,1700]
-grp_values=[1,5000]
+grp_values=[1,3000]
 
 #IDEA: Estudiar nº de capas óptima en GNN con grafos de entre 100000-50000 nodos(valores del artículo)
 for i,j in zip(grp_values,index_values):
     # Construir el comando con el valor de i
     print(f"###########BETWEENNESS PARA {G_TYPE} , CON {HYPER} = {i} ################")
     #RESETEO DE GRAFOS#--------------------------------------
-    subprocess.run(f"python central_generate_graph.py --num_graphs 15 --min_nodes 5000 --max_nodes 5000 --g_type {G_TYPE} --{HYPER} {i} --centrality bet", shell=True,cwd="datasets")
-    subprocess.run(f"python central_create_dataset.py --split_train 5 --split_test 10 --model_size 5000 --num_copies 20 --g_type {G_TYPE} --centrality bet" ,shell=True,cwd="datasets")
+    subprocess.run(f"python central_generate_graph.py --num_graphs 15 --min_nodes 3000 --max_nodes 3001 --g_type {G_TYPE} --{HYPER} {i} --centrality bet", shell=True,cwd="datasets")
+    subprocess.run(f"python central_create_dataset.py --split_train 5 --split_test 10 --model_size 3001 --num_copies 20 --g_type {G_TYPE} --centrality bet" ,shell=True,cwd="datasets")
 
-    comando_bet = f"python betweenness.py --g {G_TYPE} --num_intermediate_layer 5 --model_size 50000 --optional_name {HYPER}{j} --g_hype {i}"
+    comando_bet = f"python betweenness.py --g {G_TYPE} --num_intermediate_layer 5 --model_size 3001 --optional_name {HYPER}{j} --g_hype {i}"
     
     # Ejecutar el comando
     call_subprocess(comando_bet)
